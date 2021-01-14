f741fe78ed6ce1a730daee285af827d424242c67
takuya_endu <takuya.e.0702@gmail.com>
2021-01-14 11:07:40 +0900

    postモデルとテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_14_012800) do
+ActiveRecord::Schema.define(version: 2021_01_14_020226) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.text "content", null: false
+    t.bigint "user_id"
+    t.bigint "subscription_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["subscription_id"], name: "index_posts_on_subscription_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "subscriptions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "service_name", null: false
@@ -46,6 +56,8 @@ ActiveRecord::Schema.define(version: 2021_01_14_012800) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "subscriptions"
+  add_foreign_key "posts", "users"
   add_foreign_key "user_contracts", "subscriptions"
   add_foreign_key "user_contracts", "users"
 end
