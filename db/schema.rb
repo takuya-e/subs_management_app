2a01dc7ee3350afbc5bbe100f60cdf9418441d5b
takuya_endu <takuya.e.0702@gmail.com>
2021-01-14 11:10:07 +0900

    likeモデルとテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_14_020226) do
+ActiveRecord::Schema.define(version: 2021_01_14_020806) do
+
+  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.text "content", null: false
@@ -56,6 +65,8 @@ ActiveRecord::Schema.define(version: 2021_01_14_020226) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "subscriptions"
   add_foreign_key "posts", "users"
   add_foreign_key "user_contracts", "subscriptions"
