325de4a1a0b6331b57c2fcd53830bbd261b0f755
takuya_endu <takuya.e.0702@gmail.com>
2021-01-14 11:01:06 +0900

    user_contractモデルとテーブル作成

---
+++ app/models/user_contract.rb
@@ -0,0 +1,2 @@
+class UserContract < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_14_010835) do
+ActiveRecord::Schema.define(version: 2021_01_14_012800) do
 
   create_table "subscriptions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "service_name", null: false
@@ -21,6 +21,17 @@ ActiveRecord::Schema.define(version: 2021_01_14_010835) do
     t.float "annual_fee"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_contracts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.date "contract_date", null: false
+    t.date "cancellation_date"
+    t.bigint "user_id"
+    t.bigint "subscription_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["subscription_id"], name: "index_user_contracts_on_subscription_id"
+    t.index ["user_id"], name: "index_user_contracts_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
@@ -35,4 +46,6 @@ ActiveRecord::Schema.define(version: 2021_01_14_010835) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "user_contracts", "subscriptions"
+  add_foreign_key "user_contracts", "users"
 end
