1ab5f3e326ffd0d8abf9f7a88055cd21cd1c8b5f
takuya_endu <takuya.e.0702@gmail.com>
2021-01-17 14:53:59 +0900

    ゲストログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "homes#index"
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   resources :posts
 end
