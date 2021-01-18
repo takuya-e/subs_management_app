856fc1267d14a02d10c16edbb71013bf98740769
takuya_endu <takuya.e.0702@gmail.com>
2021-01-18 19:02:10 +0900

    未ログインでは操作できないように対応

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
+  resources :users, only: [:show]
   resources :posts
 end
