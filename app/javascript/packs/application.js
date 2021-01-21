a4a6f8e39be72d0ce2ef199ee37672f84e38f785
takuya_endu <takuya.e.0702@gmail.com>
2021-01-21 18:26:00 +0900

    FontAwesomeのインポート

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("bootstrap/dist/js/bootstrap")
+import "bootstrap/dist/js/bootstrap"
+import "@fortawesome/fontawesome-free/js/all"
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
