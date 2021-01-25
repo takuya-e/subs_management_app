d318de1ee5f9af4c124bd8199d455ec058173966
takuya_endu <takuya.e.0702@gmail.com>
2021-01-25 16:59:36 +0900

    homeデザインの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,14 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :index
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
-  
+
+  #登録時のnameの許可
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-
+  #ログイン後の飛び先指定
   def after_sign_in_path_for(resource)
     user_path(resource.id)
   end
